ac9b3091bb75007b6b836db82a232e1ab88356ee
samad324 <a.s324889@gmail.com>
2023-03-17 17:53:25 +0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
-import * as cocossd from '@tensorflow-models/coco-ssd';
+import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
+import { drawRect, getObjectCount, speakLength } from "./utilities";
+
+import logo from "./assets/images/logo.png";
+
 import "./App.css";
-import { drawRect } from "./utilities";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const msg = new SpeechSynthesisUtterance()
+  const [objects, setObjects] = useState([]);
+
+  useEffect(() => {
+    runCoco();
+  }, []);
+
+  useEffect(() => {
+    speakLength(objects);
+  }, [objects]);
+
   const runCoco = async () => {
     const net = await cocossd.load();
     setInterval(() => {
@@ -35,51 +48,44 @@ function App() {
       const obj = await net.detect(video);
       const ctx = canvasRef.current.getContext("2d");
 
-      // Text to speach un-comment the next 3 lines
-      // msg.text = `${obj.length} object detected`
-      // msg.rate = 0.7
-      // window.speechSynthesis.speak(msg)  
-      drawRect(obj, ctx)  
+      drawRect(obj, ctx);
+      if (obj.length !== 0 && objects.length !== obj.length) {
+        setObjects(obj);
+      }
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
-
+  const canvasStyle = {
+    position: "absolute",
+    marginLeft: "auto",
+    marginRight: "auto",
+    top: 160,
+    left: 0,
+    right: 0,
+    textAlign: "center",
+    zindex: 9,
+    width: 640,
+    height: 480,
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          muted={true} 
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 8,
-            width: 640,
-            height: 480,
-          }}
-        />
+      <header className="AppHeader">
+        <img src={logo} className="logo" alt="app-logo"/>
+        <h4>Team Armstrong</h4>
       </header>
+      <Webcam ref={webcamRef} muted={true} style={canvasStyle} />
+      <div className="mt-2">
+        {objects.length ? (
+          <div>
+            <h4>{getObjectCount(objects)}</h4>
+            <p>Press space bar to read the count</p>
+          </div>
+        ) : null}
+      </div>
+      <div className="canvasDiv">
+        <canvas ref={canvasRef} style={canvasStyle} />
+      </div>
     </div>
   );
 }
--- src/utilities.js
+++ src/utilities.js
@@ -1,17 +1,58 @@
-export const drawRect = (detections, ctx) =>{
-    detections.forEach(prediction => {
-        
-      const [x, y, width, height] = prediction['bbox']; 
-      const text = prediction['class']; 
+export const drawRect = (detections, ctx) => {
+  detections.forEach((prediction) => {
+    const [x, y, width, height] = prediction["bbox"];
 
-      ctx.strokeStyle = '#' + 'ffffff'
-      ctx.font = '18px Arial';
-  
-      // Draw rectangles and text
-      ctx.beginPath();   
-      ctx.fillStyle = '#' + 'ffffff'
-      ctx.fillText(text, x, y);
-      ctx.rect(x, y, width, height); 
-      ctx.stroke();
-    });
+    const text = `Object name: ${getObjectName(
+      prediction
+    )}, Confidence level: ${getScore(prediction)}%`;
+
+    //Styling text
+    ctx.strokeStyle = "#ffffff";
+    ctx.font = "22px bold";
+
+    // Draw rectangles and text
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillText(text, x, y - 4);
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+  });
+};
+
+const getScore = (prediction) => (prediction.score * 100).toFixed(2);
+
+const getObjectName = (prediction) => {
+  const str = prediction.class;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const getObjectCount = (objects) => {
+  const indivisualObjects = {};
+  let str = "";
+  for (let i = 0; i < objects.length; i++) {
+    const objectName = objects[i].class;
+    if (!indivisualObjects[objectName]) indivisualObjects[objectName] = 0;
+    indivisualObjects[objectName] += 1;
   }
+
+  for (const key in indivisualObjects) {
+    const count = indivisualObjects[key];
+    str += `${count} ${key}${count > 1 ? "s" : ""},`;
+  }
+
+  str = str.slice(0, str.length - 1);
+  str += " detected";
+  console.log(str);
+  return str;
+};
+
+export const speakLength = (objects) => {
+  const msg = new SpeechSynthesisUtterance();
+  document.body.onkeyup = function (e) {
+    if (e.key === " " || e.code === "Space" || e.keyCode === 32) {
+      msg.text = getObjectCount(objects);
+      msg.rate = 0.9;
+      window.speechSynthesis.speak(msg);
+    }
+  };
+};
